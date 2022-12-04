a92d6c869f68465902ddfd5172c6e2e64aa972ee
montexbjeliseo <secondmtx@gmail.com>
2022-12-04 00:45:04 -0300

    Formulario crear noticia agregado

--- apps/noticias/urls.py
+++ apps/noticias/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='noticias'),
+    path('', index, name='noticias'),
+    path('crear/', CrearNoticia.as_view(), name="noticias-crear"),
 ]
