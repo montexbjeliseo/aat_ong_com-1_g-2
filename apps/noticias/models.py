71bde8050e98ad0f2b9006a923da65c2454ce0b6
montexbjeliseo <secondmtx@gmail.com>
2022-12-13 01:37:34 -0300

    Filtro de noticias

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -5,23 +5,20 @@ from django.utils.html import strip_tags
 
 class Categoria(models.Model):
 	nombre = models.CharField(max_length = 60)
-	descripcion = models.CharField(max_length = 250, null = True, blank = True)
-
 	def __str__(self):
 		return self.nombre
+	def get_absolute_url(self):
+		return reverse_lazy('noticias:index') + "?categoria="+str(self.pk)
 
 class Noticia(models.Model):
 	titulo = models.CharField(max_length = 120)
-	creado = models.DateField(auto_now_add = True)
+	creado = models.DateTimeField(auto_now_add = True)
 	cuerpo = models.TextField()
 	autor = models.ForeignKey(Usuario, models.SET_NULL, null = True)
 	imagen = models.ImageField(upload_to = 'noticias', null=True, blank = True)
 	categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE, null = True)
 	visitas = models.IntegerField(default=0)
 	
-	class Meta:
-		ordering = ["-creado"]
-
 	def __str__(self):
 		return self.titulo
 
