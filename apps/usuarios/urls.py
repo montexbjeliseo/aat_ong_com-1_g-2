35871f4a4cff09a05402768f78095b9a1eeb652c
35992941 <fedezeiza32@gmail.com>
2022-12-17 02:13:05 -0300

    Seccion perfil

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('registro', views.register, name='registro'),
     path('login', views.login, name='login'),
-    path('logout', views.logout, name='logout')
+    path('logout', views.logout, name='logout'),
+    path('<str:username>/perfil',views.PerfilVista.as_view(),name='perfil'),
 ]
