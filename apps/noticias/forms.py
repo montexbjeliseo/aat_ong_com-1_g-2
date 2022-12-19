71bfa4515c5f9f1fe03aafcc219b13fde8861ae0
montexbjeliseo <secondmtx@gmail.com>
2022-12-19 14:34:59 -0300

    Limpieza

--- apps/noticias/forms.py
+++ apps/noticias/forms.py
@@ -1,6 +1,7 @@
 from django.forms import *
 from .models import *
 from django.utils.html import strip_tags
+from django.db import models
 
 class NuevaNoticiaForm(ModelForm):
     titulo = CharField(label='Titulo', required=True, widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Titulo de la noticia'}))
@@ -9,6 +10,8 @@ class NuevaNoticiaForm(ModelForm):
         'placeholder': 'Cuerpo de la noticia'
         }))
     imagen = ImageField(label="Imagen", widget=FileInput(attrs={'class': 'form-control'}))
+    categoria = ModelChoiceField(queryset=Categoria.objects.all(), required=False, widget=Select(attrs={'onchange': 'javascript:alternar_visibilidad_nueva_categoria();'}))
+    nueva_categoria = CharField(required=False, widget=TextInput(attrs={'placeholder': 'Nombre nueva categoria'}))
     class Meta:
         model = Noticia
         fields = [
@@ -23,6 +26,16 @@ class NuevaNoticiaForm(ModelForm):
             raise ValidationError('El mínimo de caracteres es de 50')
         return d_cuerpo
 
+    def clean_categoria(self):
+        d_categoria = self.cleaned_data['categoria']
+        d_nueva_categoria = self.data['nueva_categoria']
+        if not d_categoria and not d_nueva_categoria:
+            raise ValidationError('Categoria es obligatoria')
+        elif d_nueva_categoria:
+            categoria = Categoria(nombre=d_nueva_categoria)
+            categoria.save()
+            return categoria
+        return d_categoria
 class NuevaCategoriaForm(ModelForm):
     nombre = CharField(label='Nombre de la categoria', required=True, widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de la categoría'}))
     descripcion = CharField(label='Descripcion', required=True, widget=Textarea(attrs={
