2e40baa5f817d8f82b241959dfa52a00105b2a52
MontexBJEliseo <secondmtx@gmail.com>
2022-12-20 14:08:38 +0000

    Registro, arreglado

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -1,41 +1,40 @@
-from django import forms
 from django.forms import *
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField
-
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
 from .models import Usuario
+from django.db import models
 
 class RegisterUserForm(UserCreationForm):
     username = CharField(label='Nombre de usuario', required=True, widget=TextInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Nombre de usuario'}))
     email = EmailField(label='Email', required=True, widget=TextInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Email'}))
     first_name = CharField(label='Nombre', required=True, widget=TextInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Nombre'}))
     last_name = CharField(label='Apellido', required=True, widget=TextInput(attrs={
-        'class': 'form-control', 
+        'class': 'form-control',
         'placeholder': 'Apellido'}))
     password1 = CharField(
         label='Contraseña', widget=PasswordInput(attrs={
-            'class': 'form-control', 
+            'class': 'form-control',
             'placeholder': 'Contraseña'}), required=True)
     password2 = CharField(
         label='Confirmar Contraseña', widget=PasswordInput(attrs={
-            'class': 'form-control', 
+            'class': 'form-control',
             'placeholder': 'Confirmar contraseña'}), required=True)
 
-class Meta:
-    model = Usuario
-    fields = [
-        'first_name',
-        'last_name',
-        'username',
-        'email',
-        'password1',
-        'password2'
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
         ]
 
 class LoginUserForm(AuthenticationForm):
@@ -46,10 +45,10 @@ class LoginUserForm(AuthenticationForm):
             'placeholder': 'Contraseña'
         }
     ))
-class Meta:
-    fields = [
-        'username',
-        'password'
+    class Meta:
+        fields = [
+            'username',
+            'password'
         ]
 
 
